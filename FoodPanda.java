8b9a41f215fa4d9072e1108a2ce563aa2e742f7f
VIJAY-XWORKZ <abvijay.xworkz@gmail.com>
2023-08-21 11:39:55 +0530

    commiting mrthod related exmaple thru FoodPanda

---
+++ FoodPanda.java
@@ -0,0 +1,163 @@
+class FoodPanda {
+ public static double search (String foodName ) {
+   double price =0.0;
+		   System.out.println("invoked search: foodName (String)");
+		    
+				 if (foodName == "salted fish") {
+			   price=280.00;
+                return price;}
+				 if (foodName == "squid") {
+			    price=260.00;
+                return price;}
+				if (foodName == "shrimps") {
+			    price=290.00;
+                return price;}
+				 if (foodName == "salted egg") {
+			   price=260.00;
+               return price;}
+               if (foodName == "sweet chill shrimps ") {
+			    price=90.00;
+                 return price;}
+				 if (foodName == "fried squid") {
+			   price=160.00;
+                return price;}
+				 if (foodName == "bihon") {
+			    price=320.00;
+                 return price;}
+				 if (foodName == "cantoon noodles") {
+			   price=60.00;
+                return price;}
+				 if (foodName == "brocoli with garlic") {
+			    price=230.00;
+                return price;}
+				if (foodName == "baguio beans") {
+			    price=260.00;
+                return price;}
+				 if (foodName == "chopsuey") {
+			   price=285.00;
+               return price;}
+               if (foodName == "yang chow f rice") {
+			    price=90.00;
+                 return price;}
+		   if (foodName == "bday noodles") {
+			   price=160.00;
+               return price;}
+				 if (foodName == "beef brisket ") {
+			    price=310.00;
+                return price;}
+				if (foodName == "beef with brocoli") {
+			    price=285.00;
+                return price;}
+				 if (foodName == "sweet and sour pork") {
+			   price=295.00;
+               return price;}
+               if (foodName == "spareribs") {
+			    price=190.00;
+                 return price;}
+				 if (foodName == "lins chicken") {
+			   price=180.00;
+                return price;}
+				 if (foodName == "lemon chicken") {
+			    price=199.00;
+                return price;}
+				if (foodName == "fish fillet") {
+			    price=100.00;
+                return price;}
+				if (foodName == "sweet & sour fish") {
+			   price=160.00;
+                return price;}
+				 if (foodName == "corn soup") {
+			    price=199.00;
+                return price;}
+				if (foodName == "sour soup") {
+			    price=199.00;
+                return price;}
+				 if (foodName == "hot soup") {
+			   price=199.0;
+               return price;}
+               if (foodName == "spinach soup") {
+			    price=239.00;
+                 return price;}
+				 if (foodName == "wonton noodle soup") {
+			   price=239.00;
+                return price;}
+				 if (foodName == "beef with wonton n soup") {
+			    price=299.00;
+                return price;}
+				if (foodName == "pork siomal") {
+			    price=450.00;
+                return price;}
+				 if (foodName == "chicken feet") {
+			   price=600.00;
+               return price;}
+               if (foodName == "hakaw") {
+			    price=180.00;
+                 return price;}
+				
+				
+				 if (foodName == "tausi") {
+			   price=280.00;
+               return price;}
+               if (foodName == "wonton") {
+			    price=230.00;
+                 return price;}
+				 if (foodName == "egg drop") {
+			   price=285.00;
+                return price;}
+				 if (foodName == "vegetable") {
+			    price=260.00;
+                return price;}
+				if (foodName == "seaweed") {
+			    price=450.00;
+                return price;}
+				 if (foodName == "chicken noodle") {
+			   price=600.00;
+               return price;}
+               if (foodName == "chicken chop suey") {
+			    price=199.00;
+                 return price;}
+				 if (foodName == "seafood") {
+			   price=210.00;
+                return price;}
+				 if (foodName == "bbq pork") {
+			    price=199.00;
+                return price;}
+				if (foodName == "chicken") {
+			    price=260.00;
+                return price;}
+				  if (foodName == "jumbo shrimp") {
+			    price=270.00;
+                 return price;}
+				 if (foodName == "plain") {
+			   price=310.00;
+                return price;}
+				 if (foodName == "hawailan") {
+			    price=260.00;
+                return price;}
+				if (foodName == "jalapeno") {
+			    price=600.00;
+                return price;}
+				 if (foodName == "zucchini") {
+			   price=100.00;
+               return price;}
+				 if (foodName == "mushroom") {
+			    price=199.00;
+                 return price;}
+				 if (foodName == "pea pod") {
+			   price=310.00;
+                return price;}
+				 if (foodName == "orange chicken") {
+			    price=260.00;
+                return price;}
+				
+				 if (foodName == "ham") {
+			   price=600.00;
+               return price;}
+			   
+			   if (foodName == "chicken & brocoli") {
+			    price=210.00;
+                return price;}            
+              System.out.println("end of search");
+				return price;
+}
+  }
