3fe7f355adfea383b2c73146eb145c3b28544bae
VIJAY-XWORKZ <abvijay.xworkz@gmail.com>
2023-08-22 12:31:57 +0530

    metods examples

---
+++ Add.java
@@ -0,0 +1,28 @@
+class Add {
+ public static void main (String[] args ) {
+		   System.out.println("main started");
+            add(22,58);
+            add(128,78);
+			add(22,588);
+			add(554,55);
+			add(2231,252);
+			add(45,32);
+			add(897,457);
+			add(65,889);
+			add(45,52);
+			add(78,85);
+			add(52,12);
+			add(656,321);
+			add(789,654);
+			add(23,17);
+			add(45,21);
+			add(784,855);
+			add(562,44);
+			add(7852,6321);
+			add(551,489);
+			add(22,33);
+ }
+ public static void add(int a, int b){
+	  System.out.println(a+b);
+ }
+}
---
+++ Subtract.java
@@ -0,0 +1,29 @@
+class Subtract {
+public static void main (String[] args ) {
+		   System.out.println("main started");
+		   sub(45,32);
+			sub(897,457);
+			sub(65,889);
+			sub(45,52);
+			sub(78,85);
+			sub(52,12);
+			sub(656,321);
+			sub(789,654);
+			sub(123,456);
+			sub(55,12);
+			sub(548120,4851);
+			sub(5678,525);
+			sub(458,102);
+            sub(22,58);
+            sub(128,78);
+			sub(22,88);
+			sub(32,52);
+			sub(874,66);
+			sub(554,55);
+			sub(2231,252);
+			
+ }
+ public static void sub(int a, int b){
+	  System.out.println(a-b);
+ }
+}
